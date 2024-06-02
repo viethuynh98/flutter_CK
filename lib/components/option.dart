95a9a7877a1a33a048bd75cbece31c34d61f0ae9
Viet Huynh <huynhviet98@gmail.com>
2024-06-02 14:03:23 +0700

    - adjust component color

--- lib/components/option.dart
+++ lib/components/option.dart
@@ -5,7 +5,14 @@ import '../constants.dart';
 class Option extends StatelessWidget {
   const Option({
     super.key,
+    required this.text,
+    required this.index,
+    required this.onPress,
   });
+
+  final String text;
+  final int index;
+  final VoidCallback onPress;
 
   @override
   Widget build(BuildContext context) {
@@ -13,20 +20,20 @@ class Option extends StatelessWidget {
       margin: const EdgeInsets.only(top: kDefaultPadding),
       padding: const EdgeInsets.all(kDefaultPadding),
       decoration: BoxDecoration(
-          border: Border.all(color: kGrayColor),
+          border: Border.all(color: Colors.black),
           borderRadius: BorderRadius.circular(15.0)),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          const Text(
-            "1. Test",
-            style: TextStyle(color: kGrayColor, fontSize: 16),
+          Text(
+            "${index + 1} $text",
+            style: const TextStyle(color: Colors.white, fontSize: 16),
           ),
           Container(
             height: 26.0,
             width: 26.0,
             decoration: BoxDecoration(
-              border: Border.all(color: kGrayColor),
+              border: Border.all(color: Colors.white),
               borderRadius: BorderRadius.circular(15.0),
             ),
           )
