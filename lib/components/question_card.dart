b6e33bf2b2cf9a5e8e99a870a9018315702bfc29
Viet Huynh <huynhviet98@gmail.com>
2024-06-02 13:16:44 +0700

    + init class question card

---
+++ lib/components/question_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../models/questions.dart';
+import 'option.dart';
+
+class QuestionCard extends StatelessWidget {
+  const QuestionCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding)
+            .copyWith(top: 10.0, bottom: 10.0),
+        decoration: BoxDecoration(
+            color: Colors.green[100],
+            borderRadius: BorderRadius.circular(25.0)),
+        child: Column(
+          children: [
+            const SizedBox(height: 10.0),
+            Text(sample_data[0]['question'],
+                style: const TextStyle(color: Colors.black)),
+            const SizedBox(height: kDefaultFontSize),
+            Option(),
+            Option(),
+            Option(),
+            Option(),
+          ],
+        ));
+  }
+}
+
