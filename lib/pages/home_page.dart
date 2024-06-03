8f0b9b92a21e2b566ddbc1065c7d57a234d872fa
Viet Huynh <huynhviet98@gmail.com>
2024-06-04 00:59:08 +0700

    - design home page UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,7 +23,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   TextEditingController nameController = TextEditingController();
   SharedPrefs prefs = SharedPrefs();
-  final String title = 'Welcome';
   QuestionController controller = Get.put(QuestionController());
 
   @override
@@ -39,7 +38,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     prefs.saveKeyCheck(true);
     return Scaffold(
-      // backgroundColor: Colors.greenAccent,
+      backgroundColor: const Color(0xFF3b5999).withOpacity(0.6),
       drawer: Drawer(
           child: ListView(
         children: [
@@ -109,18 +108,82 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Stack(
         children: [
+          Positioned(
+            child: Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/quiz_bg_1.png"),
+                    fit: BoxFit.cover),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 50.0,
+            child: Container(
+              width: MediaQuery.of(context).size.width - 40,
+              height: 300,
+              margin: const EdgeInsets.symmetric(horizontal: 20.0),
+              decoration: BoxDecoration(
+                  color: Colors.amber[50],
+                  borderRadius: BorderRadius.circular(15.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.white.withOpacity(0.4),
+                      blurRadius: 15.0,
+                      spreadRadius: 6.0,
+                    ),
+                  ]),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0)
+                        .copyWith(top: 10.0, bottom: 10.0),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage:
+                              AssetImage('assets/icons/icon_quiz_fast.png'),
+                        ),
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage:
+                              AssetImage('assets/icons/icon_quiz_math.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0)
+                        .copyWith(top: 10),
+                    child: const Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage:
+                              AssetImage('assets/icons/icon_quiz_logic.png'),
+                        ),
+                        CircleAvatar(
+                          radius: 60,
+                          backgroundImage: AssetImage(
+                              'assets/icons/icon_quiz_multiple_choice.png'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           Positioned.fill(
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    title,
-                    style:
-                        const TextStyle(color: Colors.orange, fontSize: 25.0),
-                  ),
-                  const SizedBox(height: 40.0),
+                  const SizedBox(height: 60.0),
                   FractionallySizedBox(
                     widthFactor: 0.6,
                     child: AppElevatedButton(
