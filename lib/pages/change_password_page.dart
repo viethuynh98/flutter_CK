6b5d1ab06a594cdc426943f7e785185178d1066b
Viet Huynh <huynhviet98@gmail.com>
2024-06-04 01:03:30 +0700

    - update change password page UI

--- lib/pages/change_password_page.dart
+++ lib/pages/change_password_page.dart
@@ -47,112 +47,134 @@ class _ChangePasswordPageState extends State<ChangePasswordPage> {
         body: Stack(
           children: [
             Positioned.fill(
-              child: SingleChildScrollView(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0)
-                      .copyWith(top: 40.0, bottom: 20.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      const Text(
-                        'Change Password',
-                        style: TextStyle(color: Colors.blue, fontSize: 33.6),
-                        textAlign: TextAlign.center,
+                child: Container(
+              color: const Color(0xFF3b5999).withOpacity(0.6),
+            )),
+            Positioned(
+              top: 50.0,
+              child: Container(
+                width: MediaQuery.of(context).size.width - 40,
+                height: 650,
+                margin: const EdgeInsets.symmetric(horizontal: 20.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(15.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.4),
+                        blurRadius: 15.0,
+                        spreadRadius: 6.0,
                       ),
-                      const SizedBox(height: 40.0),
-                      const Text(
-                        'Username',
-                        style: TextStyle(color: Colors.blue, fontSize: 16.8),
-                      ),
-                      AppTextField(
-                        controller: usernameController,
-                        isReadOnly: true,
-                        hintText: 'Enter username',
-                        prefixIcon:
-                            const Icon(Icons.person, color: Colors.grey),
-                      ),
-                      const SizedBox(height: 20.0),
-                      const Text(
-                        'Current password',
-                        style: TextStyle(color: Colors.blue, fontSize: 16.8),
-                      ),
-                      AppTextField(
-                        controller: currentPasswordController,
-                        hintText: 'Enter current password',
-                        prefixIcon:
-                            const Icon(Icons.person, color: Colors.grey),
-                      ),
-                      const SizedBox(height: 20.0),
-                      const Text(
-                        'New password',
-                        style: TextStyle(color: Colors.blue, fontSize: 16.8),
-                      ),
-                      AppTextField(
-                        controller: newPasswordController,
-                        hintText: 'Enter password',
-                        isPassword: true,
-                        prefixIcon:
-                            const Icon(Icons.password, color: Colors.grey),
-                      ),
-                      const SizedBox(height: 20.0),
-                      const Text(
-                        'Confirm password',
-                        style: TextStyle(color: Colors.blue, fontSize: 16.8),
-                      ),
-                      AppTextField(
-                        controller: confirmPasswordController,
-                        hintText: 'Enter confirm password',
-                        action: TextInputAction.done,
-                        isPassword: true,
-                        prefixIcon:
-                            const Icon(Icons.password, color: Colors.grey),
-                      ),
-                      const SizedBox(height: 40.0),
-                      FractionallySizedBox(
-                        widthFactor: 0.4,
-                        child: AppElevatedButton(
-                          text: 'Next',
-                          onPressed: () {
-                            String result = 'Enter your password';
-                            if (currentPasswordController.text.trim().isEmpty) {
-                              showNotification(context, result);
-                            } else if (newPasswordController.text.isEmpty) {
-                              result = 'Enter your new password';
-                              showNotification(context, result);
-                            } else if (newPasswordController.text.isEmpty) {
-                              result = 'Confirm your new password';
-                              showNotification(context, result);
-                            }
-                            if (currentPasswordController.text ==
-                                user.password) {
-                              if (currentPasswordController.text ==
-                                  newPasswordController.text) {
-                                result =
-                                    'New Password must different from current password';
+                    ]),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20.0)
+                        .copyWith(top: 40.0, bottom: 20.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        const Text(
+                          'Change Password',
+                          style: TextStyle(color: Colors.blue, fontSize: 33.6),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 40.0),
+                        const Text(
+                          'Username',
+                          style: TextStyle(color: Colors.blue, fontSize: 16.8),
+                        ),
+                        AppTextField(
+                          controller: usernameController,
+                          isReadOnly: true,
+                          hintText: 'Enter username',
+                          prefixIcon:
+                              const Icon(Icons.person, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20.0),
+                        const Text(
+                          'Current password',
+                          style: TextStyle(color: Colors.blue, fontSize: 16.8),
+                        ),
+                        AppTextField(
+                          controller: currentPasswordController,
+                          hintText: 'Enter current password',
+                          prefixIcon:
+                              const Icon(Icons.person, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20.0),
+                        const Text(
+                          'New password',
+                          style: TextStyle(color: Colors.blue, fontSize: 16.8),
+                        ),
+                        AppTextField(
+                          controller: newPasswordController,
+                          hintText: 'Enter password',
+                          isPassword: true,
+                          prefixIcon:
+                              const Icon(Icons.password, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 20.0),
+                        const Text(
+                          'Confirm password',
+                          style: TextStyle(color: Colors.blue, fontSize: 16.8),
+                        ),
+                        AppTextField(
+                          controller: confirmPasswordController,
+                          hintText: 'Enter confirm password',
+                          action: TextInputAction.done,
+                          isPassword: true,
+                          prefixIcon:
+                              const Icon(Icons.password, color: Colors.grey),
+                        ),
+                        const SizedBox(height: 40.0),
+                        FractionallySizedBox(
+                          widthFactor: 0.4,
+                          child: AppElevatedButton(
+                            text: 'Next',
+                            onPressed: () {
+                              String result = 'Enter your password';
+                              if (currentPasswordController.text
+                                  .trim()
+                                  .isEmpty) {
                                 showNotification(context, result);
-                              } else if (newPasswordController.text ==
-                                  confirmPasswordController.text) {
-                                user = UserModel()
-                                  ..username = user.username
-                                  ..password = newPasswordController.text;
-                                result = 'Confirmed!';
-                                prefs.saveUser(user);
-
-                                Route route = MaterialPageRoute(
-                                    builder: (context) => LoginPage(
-                                          username: widget.username,
-                                        ));
-                                Navigator.push(context, route);
+                              } else if (newPasswordController.text.isEmpty) {
+                                result = 'Enter your new password';
+                                showNotification(context, result);
+                              } else if (newPasswordController.text.isEmpty) {
+                                result = 'Confirm your new password';
                                 showNotification(context, result);
                               }
-                            } else {
-                              result = 'Wrong password';
-                              showNotification(context, result);
-                            }
-                          },
-                        ),
-                      )
-                    ],
+                              if (currentPasswordController.text ==
+                                  user.password) {
+                                if (currentPasswordController.text ==
+                                    newPasswordController.text) {
+                                  result =
+                                      'New Password must different from current password';
+                                  showNotification(context, result);
+                                } else if (newPasswordController.text ==
+                                    confirmPasswordController.text) {
+                                  user = UserModel()
+                                    ..username = user.username
+                                    ..password = newPasswordController.text;
+                                  result = 'Confirmed!';
+                                  prefs.saveUser(user);
+
+                                  Route route = MaterialPageRoute(
+                                      builder: (context) => LoginPage(
+                                            username: widget.username,
+                                          ));
+                                  Navigator.push(context, route);
+                                  showNotification(context, result);
+                                }
+                              } else {
+                                result = 'Wrong password';
+                                showNotification(context, result);
+                              }
+                            },
+                          ),
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ),
